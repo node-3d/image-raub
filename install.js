068b99589612e4addc672f7f05ccb63d50cf9fbe
Luis Blanco <luisblanco1337@gmail.com>
2021-05-07 22:15:58 +0300

    Update install

--- install.js
+++ install.js
@@ -4,6 +4,6 @@ const install = require('addon-tools-raub/install');
 
 
 const prefix = 'https://github.com/node-3d/image-raub/releases/download';
-const tag    = 'v3.2.0-pre';
+const tag    = 'v3.2.0';
 
 install(`${prefix}/${tag}`);
