8030199c61de4d67249523d72ad8be15bfc72fef
Luis Blanco <luisblanco1337@gmail.com>
2023-01-07 19:47:18 +0400

    Log libs

--- core.js
+++ core.js
@@ -5,6 +5,9 @@ require('segfault-raub');
 // Add deps dll dirs
 require('deps-freeimage-raub');
 
+console.log('dd', process.env.PATH);
+console.log('dd2', __dirname);
+
 const { getBin } = require('addon-tools-raub');
 
 const core = require(`./${getBin()}/image`);
