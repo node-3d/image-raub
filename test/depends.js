be58d63434e0a88fcd250d882de77a03747bb6b5
Luis Blanco <luisblanco1337@gmail.com>
2023-01-07 20:54:15 +0400

    Try depends

---
+++ test/depends.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const path = require('node:path');
+const util = require('node:util');
+const exec = util.promisify(require('node:child_process').exec);
+
+const {
+	exists, copy,
+} = require('addon-tools-raub');
+
+const fail = (error) => {
+	console.error(error);
+	process.exit(-1);
+};
+
+
+(async () => {
+	try {
+		console.log('DEPENDS');
+		await copy(path.resolve('../bin-windows/image.node'), path.resolve('./image.node'));
+		
+		const { stderr } = await exec('start cmd /k depends /c /u:1 /pl:1 pf:1 /ot:dep.txt image.node');
+		if (stderr) {
+			console.error(stderr);
+		}
+		
+		// eslint-disable-next-line no-constant-condition
+		while (true) {
+			await new Promise((res) => setTimeout(res, 3000));
+			const isFound = await exists('dep.txt');
+			
+			console.log('isFound:', isFound);
+			
+			if (isFound) {
+				break;
+			}
+		}
+		
+		console.log('-------------------');
+	} catch (error) {
+		fail(error);
+	}
+})();
