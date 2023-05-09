ae4d26a87a01b7924cecaf00a4335652f766c997
Luis Blanco <luisblanco1337@gmail.com>
2023-05-09 20:31:38 +0400

    Cleanup debug tools

--- core.js
+++ core.js
@@ -5,9 +5,6 @@ require('segfault-raub');
 // Add deps dll dirs
 require('deps-freeimage-raub');
 
-console.log('dd', process.env.PATH);
-console.log('dd2', __dirname);
-
 const { getBin } = require('addon-tools-raub');
 
 const core = require(`./${getBin()}/image`);module.exports = core;
